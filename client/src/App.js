bb550eaaae0533e3d260ba9c2d4a92913aff0819
mmfKupl <ikuplevich@mail.ru>
2019-04-10 12:44:04 +0300

    rebuild bgtemplate

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./App.css";
 
 import Button from "./Elements/Button/Button";
 import BGTemplate from "./Pages/StartPage/BGTemplate";
-import HeaderTitle from "./Elements/HeaderTitle/HeaderTitle";
 import Input from "./Elements/Input/Input";
 import LinkText from "./Elements/LinkText/LinkText";
 import Text from "./Elements/Text/Text";
@@ -11,13 +11,14 @@ import Text from "./Elements/Text/Text";
 class App extends Component {
   render() {
     return (
-      <BGTemplate>
-        <HeaderTitle />
-        <Text text="kuku" />
-        <Input />
-        <Button text="Button" />
-        <LinkText href="to" text="to" />
-      </BGTemplate>
+      <Router>
+        <BGTemplate>
+          <Text text="kuku" />
+          <Input />
+          <Button text="Button" />
+          <LinkText href="to" text="to" />
+        </BGTemplate>
+      </Router>
     );
   }
 }
--- client/src/Pages/StartPage/BGTemplate.js
+++ client/src/Pages/StartPage/BGTemplate.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
 import "./BGTemplate.css";
 
+import HeaderTitle from "../../Elements/HeaderTitle/HeaderTitle";
+
 class BGTemplate extends Component {
   render() {
-    return <div className="backround_wrapper">{this.props.children}</div>;
+    return (
+      <div className="backround_wrapper">
+        <HeaderTitle />
+        {this.props.children}
+      </div>
+    );
   }
 }
 
