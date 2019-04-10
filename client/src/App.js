dad62b901987cea76bf42a2c988d060f5d96ec27
mmfKupl <ikuplevich@mail.ru>
2019-04-10 11:22:28 +0300

    update BGTemplate

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import Button from "./Elements/Button/Button";
 import BGTemplate from "./Pages/StartPage/BGTemplate";
 
 class App extends Component {
   render() {
     return (
       <BGTemplate>
-        AA<p>D</p>
+        <Button text="ПРИВЕТ" />
       </BGTemplate>
     );
   }
