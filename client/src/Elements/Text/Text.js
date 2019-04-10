3be8662c8b37f72c2642e05e5df7965b6c0a237d
mmfKupl <ikuplevich@mail.ru>
2019-04-10 12:32:09 +0300

    upd

--- client/src/Elements/Text/Text.js
+++ client/src/Elements/Text/Text.js
@@ -2,11 +2,7 @@ import React from "react";
 import "./Text.css";
 
 const Text = props => {
-    return (
-        <p className="text">
-            {props.Text}
-        </p>
-    );
-  }
+  return <p className="text">{props.text}</p>;
+};
 
 export default Text;
