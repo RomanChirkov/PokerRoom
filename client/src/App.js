4b7b1de9bce6bee2c51d7226fa98df0728ee2264
fulelgupport <chirk0ff.poker@gmail.com>
2019-04-10 13:35:07 +0300

    )

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import BGtempate from "./Pages/StartPage/BGTemplate";
+import Login from "./Pages/StartPage/Login/Login";
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <Route exact path="/" component={} />
+        <Route exact path="/" component={Login} />
       </Router>
     );
   }
---
+++ client/src/Pages/StartPage/Login/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { BrowserRouter as Link } from "react-router-dom";
+
+import BGtemplate from "../BGTemplate";
+
+class Login extends Component {
+  render() {
+    return (
+        <BGtemplate/>
+    );
+  }
+}
+
+export default Login;
