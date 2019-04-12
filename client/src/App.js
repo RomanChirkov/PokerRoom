91b999be4a96caeef89ee058c9cb4655f0386881
mmfKupl <ikuplevich@mail.ru>
2019-04-12 17:02:03 +0300

    update path !!

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,9 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { connect } from "react-redux";
 
-import SignUp from "./Pages/StartPage/SignUp/SignUp";
+import SignUp from "./pages/StartPage/SignUp/SignUp";
 import LogIn from "./pages/StartPage/LogIn/LogIn";
-import ResetPassword from "./Pages/StartPage/ResetPassword/ResetPassword";
+import ResetPassword from "./pages/StartPage/ResetPassword/ResetPassword";
 
 import { testAction, test1Action } from "./actions/testAction";
 
