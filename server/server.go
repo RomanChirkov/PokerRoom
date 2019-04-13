02cd2d91de1002071a9d7861a741697a72f26143
fulelgupport <chirk0ff.poker@gmail.com>
2019-04-13 09:00:29 +0300

    add bd

--- server/server.go
+++ server/server.go
@@ -1,20 +1,59 @@
 package main
 
 import (
+	"os"
+	"bufio"
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
 	"strings"
-	"log"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
-func HomeRouterHandler(w http.ResponseWriter, r *http.Request){
+type User struct { 
+	id        int
+	login    string
+	password string
+}
+
+var database *sql.DB
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	rows, err := database.Query("select * from serverbd.users")
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("error")
+	}
+	fmt.Println(rows)
+	defer rows.Close()
+	users := []User{}
+	str := ""
+	for rows.Next() {
+		p := User{}
+		err := rows.Scan(&p.id, &p.login, &p.password)
+		if err != nil{
+			fmt. Println(err)
+			continue
+		}
+		users = append(users,p)
+		str += "one"
+	}
+	fmt.Fprintf(w, "lol")
+	fmt.Println(users)
+
+	// fmt.Fprintf(w, str)
+}
+
+func HomeRouterHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	fmt.Println("scheme", r.URL.Scheme)
 
 	fmt.Println(r.Form["url_long"])
-	for k,v :=range r.Form{
+	for k, v := range r.Form {
 		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v,""))
+		fmt.Println("val:", strings.Join(v, ""))
 	}
 	fmt.Fprintf(w, "PokerRoom")
 }
@@ -22,12 +61,48 @@ func HomeRouterHandler(w http.ResponseWriter, r *http.Request){
 func apiHendler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "hellow, there is api page")
 }
+func getConfig() (config []string)  {
+	file, err := os.Open("./db.config")
+	if err != nil {
+		panic("Ошибка при чтении конфига:" + err.Error())
+	}
+	defer file.Close()
+
+	var data string
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan(){
+		data = scanner.Text()
+	}
+	config = strings.Split(data, ",")
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+	} 
+
+	return 
+}
+func init(){
+	conf := getConfig()
+	fmt.Println("Connecting to database...")
+// connString := "username:password@tcp(sql.hosting.com:3306)/databaseName"
+	// connString := "root:75gasina@/serverbd"
+	connString := fmt.Sprintf("%s:%s@/serverbd", conf[0], conf[1])
+	db, err := sql.Open("mysql", connString)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println(db)
+	database = db
+}
 
 func main() {
+	defer database.Close()
+	http.HandleFunc("/api/bd", IndexHandler)
 	http.HandleFunc("/", HomeRouterHandler)
 	http.HandleFunc("/api", apiHendler)
 	err := http.ListenAndServe(":9000", nil)
-	if err != nil{
+	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
